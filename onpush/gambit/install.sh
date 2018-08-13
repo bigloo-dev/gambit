1e11dbbff48926f30fe94fa86afd127f27c512d5
Manuel Serrano <Manuel.Serrano@inria.fr>
2018-08-13 14:53:59 +0200

    .

--- onpush/gambit/install.sh
+++ onpush/gambit/install.sh
@@ -4,7 +4,7 @@
 
 if [ ! -z "`git diff HEAD HEAD^ ./configure`" -o ! -f config.log -o ! -d $ONPUSH_DIR/local ]; then
   echo "configuring gambit..."
-  ./configure --enable-single-host --prefix=$ONPUSH_DIR/local && exit 1
+  ./configure --enable-single-host --prefix=$ONPUSH_DIR/local || exit 1
 fi
 
 echo "compiling gambit... "
