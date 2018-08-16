403c976c0c8b383fe816c06862d6c1f54225810e
Manuel Serrano <Manuel.Serrano@inria.fr>
2018-08-16 08:27:24 +0200

    .

--- onpush/commit.sh
+++ onpush/commit.sh
@@ -7,7 +7,8 @@ origin=`git remote -v | grep "(push)" | awk '{print $1}'`
 BRANCH=`echo -n $BRANCH | sed 's|refs/heads/||'`
 
 function restore() {
-  git checkout $BRANCH  
+  git checkout $BRANCH
+  make deselect-gen-for-commit
 }
 
 trap restore EXIT
